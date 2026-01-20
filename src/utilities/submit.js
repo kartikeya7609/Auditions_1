54cf867122f31bae867272d1b6266bbe1588fe34
kartikeya7609 <kartikeyakk2007@gmail.com>
2026-01-20 15:22:25 +0530

    modified submit.js

--- src/utilities/submit.js
+++ src/utilities/submit.js
@@ -1,13 +1,17 @@
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase-config";
-import { collection, addDoc } from "firebase/firestore";
 
-async function Submit(data) {
+const Submit = async (formData) => {
   try {
-    await addDoc(collection(db, "registrations"), data);
-    console.log("Submitted");
-  } catch (e) {
-    console.log(e);
+    await addDoc(collection(db, "ieee_applications"), {
+      ...formData,
+      createdAt: serverTimestamp(),
+    });
+
+    console.log("Form submitted successfully");
+  } catch (error) {
+    console.error("Error submitting form:", error);
   }
-}
+};
 
 export default Submit;
